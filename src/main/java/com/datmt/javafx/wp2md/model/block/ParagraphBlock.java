934a87020c1ad9a9eeb458a6831f72df0074d578
dtt <t2dx.inc@gmail.com>
2022-09-27 21:17:00 +0700

    parse to MD link

--- src/main/java/com/datmt/javafx/wp2md/model/block/ParagraphBlock.java
+++ src/main/java/com/datmt/javafx/wp2md/model/block/ParagraphBlock.java
@@ -5,6 +5,7 @@ import com.datmt.javafx.wp2md.Line;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ParagraphBlock extends Block {
@@ -17,7 +18,32 @@ public class ParagraphBlock extends Block {
         Element element = Jsoup.parse(getContent()).selectFirst("p");
 
         assert element != null;
-        return element.html()
+        var links = element.select("a");
+
+        var mdLinks = new ArrayList<String>();
+
+       for (Element e : links)  {
+          var mdL = new StringBuilder() ;
+          var linkText = e.text();
+          var linkHref = e.attr("href");
+          var linkTitle = e.attr("title");
+
+          mdL.append("[").append(linkText).append("](") .append(linkHref)
+                  .append(linkTitle.isEmpty() ? linkTitle : " \"" + linkTitle +  "\"")
+                  .append(")");
+
+          mdLinks.add(mdL.toString());
+       }
+
+       var content = element.html();
+
+
+       for (int i = 0; i < links.size(); i ++) {
+          content = content.replace(links.get(i).outerHtml(), mdLinks.get(i));
+       }
+
+
+        return content
                 .replaceAll("<code>", "`")
                 .replaceAll("</code>", "`")
                 .replaceAll("<strong>", "**")
